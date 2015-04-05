2152b3e2a8ad2f8ae07c55c6d6de7b7e2cd9055f
littlewu0 <littlewu0@gmail.com>
2015-04-05 19:57:29 +0800

    提交测试代码
    测试代码

---
+++ XJRecordEngineDemo/XJRecordEngineDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  XJRecordEngineDemo
+//
+//  Created by wxj on 15/4/5.
+//  Copyright (c) 2015年 littlewu0. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ XJRecordEngineDemo/XJRecordEngineDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  XJRecordEngineDemo
+//
+//  Created by wxj on 15/4/5.
+//  Copyright (c) 2015年 littlewu0. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
